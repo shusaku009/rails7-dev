cc9f759a7985f0f04a8c3b52002f0d82e31c7dca
shusaku009 <shusaku009@gmail.com>
2023-06-01 00:25:59 +0900

    add numbered parameter

--- ruby_book/ib/arrangement.rb
+++ ruby_book/ib/arrangement.rb
@@ -1,3 +1,15 @@
+dimensions = [
+  [10, 20],
+  [30, 40],
+  [50, 60],
+]
+# 面積の計算結果を格納する配列
+areas = []
+# 配列の要素分だけブロックパラメータを用意すると、各要素の値が別々に格納される
+dimensions.each do |length, width|
+  areas << length * width
+end
+
 # 最初のブロックパラメーターlengthに配列がまるごと渡される
 dimensions = [
   [10, 20],
