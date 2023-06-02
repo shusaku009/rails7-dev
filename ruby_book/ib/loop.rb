467e2c850dd74ca029d712a5b6bb8ce6cc387e8e
shusaku009 <shusaku009@gmail.com>
2023-06-02 19:00:57 +0900

    update loop.rb

--- ruby_book/ib/loop.rb
+++ ruby_book/ib/loop.rb
@@ -76,17 +76,146 @@ ret =
     break
   end
 
-  ret =
-  while true
-    break 123
-  end
+ret =
+while true
+  break 123
+end
 
-  fruits = ['apple', 'orange', 'melon']
-  numbers = [1, 2, 3]
-  fruits.each do |fruit|
+fruits = ['apple', 'orange', 'melon']
+numbers = [1, 2, 3]
+fruits.each do |fruit|
+  numbers.shuffle.each do |n|
+    puts "#{fruit}, #{n}"
+    # numbersのループは脱出するが、fruitsのループは継続する
+    break if n == 3
+  end
+end
+
+fruits = ['apple', 'melon', 'orange']
+numbers = [1, 2, 3]
+catch :done do
+  fruits.shuffle.each do |fruit|
     numbers.shuffle.each do |n|
       puts "#{fruit}, #{n}"
-      # numbersのループは脱出するが、fruitsのループは継続する
-      break if n == 3
+      if fruit == 'orange' && n == 3
+        # すべての繰り返し処理を脱出する
+        throw :done
+      end
     end
   end
+end
+
+fruit = ['apple', 'melon', 'orange']
+numbers = [1, 2, 3]
+catch :done do
+  fruits.shuffle.each do |fruit|
+    numbers.shuffle.each do |n|
+      puts "#{fruit}, #{n}"
+      if fruit == 'orange' && n == 3
+        # catch と一致しないタグをthrowする
+        throw :foo
+      end
+    end
+  end
+end
+
+ret =
+  catch :done do
+    throw :done
+  end
+ret
+
+ret =
+  catch :done do
+    throw :done, 123
+  end
+ret
+
+def greet(country)
+  # countryがnilならメッセージを返してメソッドを抜ける
+  return 'countryを入力してください'if country.nil?
+
+  if country == 'japan'
+    'こんにちは'
+  else
+    'hello'
+  end
+end
+
+def calc_with_break
+  numbers = [1, 2, 3, 4, 5, 6]
+  target = nil
+  numbers.shuffle.each do |n|
+    target = n
+    # breakで脱出する
+    break if n.even?
+  end
+  target * 10
+end
+calc_with_break
+
+def calc_with_return
+  numbers = [1, 2, 3, 4, 5, 6]
+  targe = nil
+  numbers.shuffle.each do |n|
+    target = n
+    # returnで脱出する？
+    return if n.even?
+  end
+  targe * 10
+end
+calc_with_return
+
+numbers = [1, 2, 3, 4, 5]
+numbers.each do |n|
+  # 偶数であれば中断して次の繰り返し処理に進む
+  next if n.even?
+  puts n
+end
+
+numbers = [1, 2, 3, 4, 5]
+i = 0
+while i < numbers.size
+  n = numbers[i]
+  i += 1
+  # while文でnextを使う
+  next if n.even?
+  puts n
+end
+
+fruits = ['apple', 'orange', 'melon']
+numbers = [1, 2, 3, 4, 5]
+fruits.each do |fruit|
+  numbers.each do |n|
+    # 繰り返し処理が入れ子になっている場合は、一番内側のループだけが中断される
+    next if n.even?
+    puts "#{fruit}, #{n}"
+  end
+end
+
+foods = ['ピーマン','トマト','セロリ']
+foods.each do |food|
+  print "#{food}は好きですか？"
+  # sampleは配列からランダムに1要素を取得するメソッド
+  answer = ['はい', 'いいえ'].sample
+  puts answer
+
+  # はいと答えがなければもう一度聞き直す
+  redo unless answer == 'はい'
+end
+
+foods = ['ピーマン','トマト','セロリ']
+count = 0
+foods.each do |food|
+  print "#{food}は好きですか？"
+  # sampleは配列からランダムに1要素を取得するメソッド
+  answer = ['はい', 'いいえ'].sample
+  puts answer
+
+  count += 1
+  # やり直しは2回までにする
+  redo if answer != 'はい' && count < 2
+
+  # カウントをリセット
+  count = 0
+end
