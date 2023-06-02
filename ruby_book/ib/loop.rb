70ea3f30ca5d7742c03c3ad17e926ec4b5cbbfdf
shusaku009 <shusaku009@gmail.com>
2023-06-02 18:23:24 +0900

    update loop.rb

--- ruby_book/ib/loop.rb
+++ ruby_book/ib/loop.rb
@@ -44,3 +44,49 @@ Range.include?(Enumerable)
 
 1.upto(3).class
 Enumerator.include?(Enumerable)
+
+numbers = [1,2,3,4,5].shuffle
+numbers.each do |n|
+  puts n
+  # 5が出たら繰り返しを脱出する
+  break if n == 5
+end
+
+numbers = [1,2,3,4,5].shuffle
+i = 0
+while i < numbers.size
+  n = numbers[i]
+  puts n
+  # 5が出たら繰り返しを脱出する
+  break if n == 5
+  i += 1
+end
+
+numbers = [1,2,3,4,5].shuffle
+i = 0
+while i < numbers.size
+  n = numbers[i]
+  puts n
+  break if n == 5
+  i += 1
+end
+
+ret = 
+  while true
+    break
+  end
+
+  ret =
+  while true
+    break 123
+  end
+
+  fruits = ['apple', 'orange', 'melon']
+  numbers = [1, 2, 3]
+  fruits.each do |fruit|
+    numbers.shuffle.each do |n|
+      puts "#{fruit}, #{n}"
+      # numbersのループは脱出するが、fruitsのループは継続する
+      break if n == 3
+    end
+  end
