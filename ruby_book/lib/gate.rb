302577c60fa55fd65bdeea43beae091de93ec559
shusaku009 <shusaku009@gmail.com>
2023-06-08 23:44:13 +0900

    update class

--- ruby_book/lib/gate.rb
+++ ruby_book/lib/gate.rb
@@ -119,3 +119,82 @@ user.is_a?(BasicObject)
 
 # is-a関係にない場合はfalse
 user.is_a?(String)
+
+class Product
+  attr_reader :name, :price
+
+  def initialize(name, price)
+    @name = name
+    @price = price
+  end
+end
+product = Product.new('A great movie', 1000)
+product.name
+product.price
+class DVD < Product
+  # nameとpriceはスーパークラスでattr_readerが設定されているので定義不要
+  attr_reader :running_time
+
+  def initialize(name, price, running_time)
+    # スーパークラスにも存在している属性
+    @name = name
+    @price = price
+    # DVDクラス独自の属性
+    @running_time = running_time
+  end
+end
+dvd = DVD.new('A great movie', 1000, 120)
+dvd.name
+dvd.price
+dvd.running_time
+
+class DVD < Product
+  # nameとpriceはスーパークラスでattr_readerが設定されているので定義不要
+  attr_reader :running_time
+
+  def initialize(name, price, running_time)
+    # スーパークラスのinitilizeメソッドを呼び出す
+    super(name,price)
+    @running_time = running_time
+  end
+end
+dvd = DVD.new('A great movie', 1000, 120)
+dvd.name
+dvd.price
+dvd.running_time
+
+class DVD < Product
+  def initialize(name, price)
+    # 引数をすべてスーパークラスのメソッドに渡す、つまりsuper(name, price)と書いたのと同じ
+    super
+
+    # サブクラスで必要な処理を書く
+  end
+end
+dvd = DVD.new('A great movie', 1000)
+dvd.name
+dvd.price
+
+class DVD < Product
+  def initialize(name, price)
+    # supser()だと引数なしでスーパークラスのメソッドを呼び出す
+    # ただし数が合わないのでエラーになる
+    super()
+
+    # サブクラスで必要な処理を書く
+  end
+end
+dvd = DVD.new('A great movie', 1000)
+dvd.name
+dvd.price
+
+class DVD < Product
+  # サブクラスで特別な処理をしないなら、同名メソッドを定義する必要はない
+  # (スーパークラスに処理を任せる)
+  # def initialize(name, price)
+  #   super
+  # end
+end
+dvd = DVD.new('A great movie', 1000)
+dvd.name
+dvd.price
