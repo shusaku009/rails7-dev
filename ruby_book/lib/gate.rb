ce92bdc3da4986d9db3560717796873ec5ad7335
shusaku009 <shusaku009@gmail.com>
2023-06-08 20:51:11 +0900

    update class

--- ruby_book/lib/gate.rb
+++ ruby_book/lib/gate.rb
@@ -53,3 +53,26 @@ class Foo
   # (クラスメソッドbarが定義された後なので、呼び出せる)
   self.bar
 end
+
+class Product
+  attr_accessor :name, :price
+
+  def initialize(name, price)
+    @name = name
+    @price = price
+  end
+
+  # 金額を整形するクラスメソッド
+  def self.format_price(price)
+    "#{price}円"
+  end
+
+  def to_s
+    # インスタンスメソッドからクラスメソッドを呼び出す
+    formatted_price = Product.format_price(price)
+    "name: #{name}, price: #{formatted_price}"
+  end
+end
+
+product = Product.new('A greet movie', 100)
+product.to_s
