24db4ba7bbcb359e1e03d79b0ad0032945dfc01f
shusaku009 <shusaku009@gmail.com>
2023-06-09 21:58:28 +0900

    update class

--- ruby_book/lib/gate.rb
+++ ruby_book/lib/gate.rb
@@ -404,3 +404,108 @@ class User
   def bar
   end
 end
+
+class Product
+  private
+
+  # private
+  def name
+    'A greate movie'
+  end
+end
+
+class DVD < Product
+  def to_s
+    "name: #{name}"
+  end
+end
+
+dvd = DVD.new
+# 内部でスーパークラスのprivateメソッドを呼んでいるがエラーにはならない
+dvd.to_s
+
+class Product
+  def to_s
+    # nameは常に"A greate movie"になるとは限らない
+    "name: #{name}"
+  end
+
+  private
+
+  def name
+    'A greate movie'
+  end
+end
+
+class DVD < Product
+  private
+
+  # スーパークラスのメソッドをオーバーライドする
+  def name
+    'An awesome film'
+  end
+end
+
+product = Product.new
+# Productクラスのnameメソッドが使われる
+product.to_s
+
+dvd = DVD.new
+dvd.to_s
+
+# nameをprivateメソッドとして定義する
+class Product
+  private
+
+  def name
+    'A greate movie'
+  end
+end
+
+# nameをpublicメソッドとしてオーバーライドする
+class DVD < Product
+  public
+
+  def name
+    'An awesome film'
+  end
+end
+
+# Productクラスのnameメソッドは呼び出せない
+product = Product.new
+product.name
+# DVDクラスのメソッドは呼び出せる
+dvd = DVD.new
+dvd.name
+
+class User
+  private
+
+  # クラスメソッドもprivateメソッドになる？
+  def self.hello
+    'Hello!'
+  end
+end
+# クラスメソッドはprivateメソッドにはならない！
+User.hello
+
+class User
+  class << self
+    # class << selfの構文であればクラスメソッドでもprivateが機能する
+    private
+
+    def hello
+      'Hello!'
+    end
+  end
+end
+User.hello
+
+class User
+  def self.hello
+    'Hello!'
+  end
+  # 後からクラスメソッドをprivateに変更する
+  private_class_method :hello
+end
+User.hello
