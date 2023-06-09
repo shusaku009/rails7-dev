b0e17c655af56b521331312cc9e1a5a649265db1
shusaku009 <shusaku009@gmail.com>
2023-06-10 08:52:01 +0900

    update class

--- ruby_book/lib/gate.rb
+++ ruby_book/lib/gate.rb
@@ -559,3 +559,40 @@ end
 user = User.new('Alice')
 user.name
 user.name = 'Bob'
+
+class User
+  # weightは外部に公開しない
+  attr_reader :name
+
+  def initialize(name, weight)
+    @name = name
+    @weight = weight
+  end
+
+  # 自分がother_userより重い場合はtrue
+  def heavier_than?(other_user)
+    other_user.weight < @weight
+  end
+
+  protected
+
+  # protectedメソッドなので同じクラスかサブクラスであればレシーバ付きで呼び出せる
+  def weight
+    @weight
+  end
+end
+
+alice = User.new('Alice', 50)
+bob = User.new('Bob', 60)
+# Aliceはbobのweightを取得できない
+alice.heavier_than?(bob)
+bob.heavier_than?(alice)
+
+# クラスの外ではweightを呼び出せない
+alice.weight
+
+class User
+  attr_reader :name
+  # weightメソッドの定義と同時にprotectedメソッドにする(Ruby 3.0以上なら有効)
+  protected attr_reader :weight
+end
