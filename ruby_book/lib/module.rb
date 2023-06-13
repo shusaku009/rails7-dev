eaa4cc8c9cde9de0ed387e95e0f38e2d3ce55ca1
shusaku009 <shusaku009@gmail.com>
2023-06-14 00:07:41 +0900

    update module

--- ruby_book/lib/module.rb
+++ ruby_book/lib/module.rb
@@ -165,3 +165,92 @@ end
 Product.include Loggable
 Product.extend Loggable
 
+module Loggable
+  # 省略
+end
+
+class Product
+  include Loggable
+  # 省略
+end
+
+Product.include?(Loggable)
+
+Product.included_modules
+
+Product.ancestors
+
+product = Product.new
+# product.classはProductクラスを返す
+product.class.include?(Loggable)
+product.class.included_modules
+
+product = Product.new
+# 引数が自クラス、includeしているモジュール、スーパークラスのいずれかに該当すればtrue
+product.is_a?(Product)
+product.is_a?(Loggable)
+product.is_a?(Object)
+
+Array.include?(Enumerable)
+Hash.include?(Enumerable)
+Range.include?(Enumerable)
+
+# 配列、ハッシュ、範囲でmapメソッドを使う
+[1,2,3].map { |n| n * 10 }
+{ a: 1, b: 2, c: 3 }.map { |k, v| [k,v * 10 ]}
+(1..3).map { |n| n * 10 }
+
+# 配列、ハッシュ、範囲でcountメソッドを使う
+[1,2,3].count
+{a: 1, b: 2, c: 3}.count
+(1..3).count
+
+2 <=> 1
+2 <=> 2
+1 <=> 2
+2 <=> 'abc'
+
+'xyz' <=> 'abc'
+'abc' <=> 'abc'
+'abc' <=> 'xyz'
+'abc' <=> 123
+
+2 > 1
+2 <= 1
+2 == 1
+
+'abc' > 'xyz'
+'abc' <= 'xyz'
+'abc' == 'xyz'
+
+class Tempo
+  include Comparable
+
+  attr_reader :bpm
+
+  def initialize(bpm)
+    @bpm = bpm
+  end
+  # <=>はComparableモジュールで使われる演算子(メソッド)
+  def <=>(other)
+    # otherがTempoであればbpm同士を<=>で比較した結果を返す
+    # それ以外は比較できないのでnilを返す
+    other.is_a?(Tempo)? bpm <=> other.bpm : nil
+  end
+
+  # irb上で結果を見やすくするためにinspectメソッドをオーバーライド
+  def inspect
+    "#{bpm}bpm"
+  end
+end
+
+t_120 = Tempo.new(120)
+t_180 = Tempo.new(180)
+
+t_120 > t_180
+t_120 <= t_180
+t_120 == t_180
+
+tempos = [Tempo.new(180), Tempo.new(60), Tempo.new(120)]
+# sortメソッドの内部では並び替えの際に<=>演算子が使われる
+tempos.sort
