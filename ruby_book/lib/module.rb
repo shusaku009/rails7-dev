7651f51f271fdd9d8e0052f1b74101b76e52c2df
shusaku009 <shusaku009@gmail.com>
2023-06-13 19:50:00 +0900

    update module

--- ruby_book/lib/module.rb
+++ ruby_book/lib/module.rb
@@ -78,3 +78,90 @@ product.title
 
 user = User.new
 user.name
+
+module Loggable
+  # logメソッドはprivateメソッドにする
+  private
+
+  def log(text)
+    puts "[LOG] #{text}"
+  end
+end
+
+class Product
+  include Loggable
+
+  def title
+    log 'title is called.'
+    'A great movie'
+  end
+end
+
+product = Product.new
+# logメソッドはprivateメソッドなので外部から呼び出せない
+product.log 'public?'
+
+module Taggable
+  def price_tag
+    # priceメソッドはinclude先で定義されているはず、という前提
+    "#{price}円"
+  end
+end
+
+class Product
+  include Taggable
+
+  def price
+    1000
+  end
+end
+
+product = Product.new
+product.price_tag
+
+class Taggable
+  def price_tag
+    # あえてselfを付けて呼び出しても良い
+    # selfはinclude先のクラス(たとえばProductクラス)のインスタンスになる
+    "#{self.price}円"
+  end
+end
+
+# モジュールの定義はincludeするときと同じ
+module Loggable
+  def log(text)
+    puts "[LOG]#{text}"
+  end
+end
+  
+class Product
+  # Loggableモジュールのメソッドを特異メソッド(クラスメソッド)として追加する
+  extend Loggable
+  
+  def self.create_products(names)
+    # logメソッドをクラスメソッド内で呼び出す
+    # (つまりlogメソッド自体もクラスメソッドになっている)
+    log 'create_products is called.'
+    # 省略
+  end
+end
+
+# クラスメソッド経由でlogメソッドが呼び出される
+Product.create_products([])
+
+# Productクラスのクラスメソッドとして直接呼び出すことも可能
+Product.log('Hello')
+
+class Product
+  extend Loggable
+
+  # logメソッドをクラス構文の直下で呼び出す
+  # (クラスが読み込まれるタイミングで、このlogメソッドも実行される)
+  log 'Defined Product class.'
+end
+
+# クラス構文の内部でinclude/extendを使う代わりに、クラス名.include.
+# またはクラス名.extendの形式でモジュールをinclude/extendする
+Product.include Loggable
+Product.extend Loggable
+
