b9285a7291cf3a68a1fbba1297ff7ed03f7dbf5b
shusaku009 <shusaku009@gmail.com>
2023-06-16 08:14:36 +0900

    update exception

--- ruby_book/lib/exception_handling.rb
+++ ruby_book/lib/exception_handling.rb
@@ -297,3 +297,26 @@ def currency_of(country)
 end
 # 例外が発生する
 currency_of(:italy)
+
+# 書き込みモードでファイルを開く
+file = File.open('some.txt', 'w')
+
+begin
+  # ファイルに文字列を書き込む
+  file << 'Hello'
+ensure
+  # 例外の有無に関わらず必ずファイルをクローズする
+  file.close
+end
+
+# ブロック付きでオープンすると、メソッドの実行後に自動的にクローズされる
+File.open('some.txt', 'w') do |file|
+  file << 'Hello'
+end
+
+File.open('some.txt', 'w') do |file|
+  file << 'Hello'
+  # わざと例外を発生させる
+  1 / 0
+end
+# 例外は発生するものの、openメソッドによってクローズ処理自体は必ず行われる
