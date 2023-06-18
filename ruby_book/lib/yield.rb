57cef4ffc349e9864a60d2b70aa915c5ece523dc
shusaku009 <shusaku009@gmail.com>
2023-06-18 17:30:15 +0900

    update yield

--- ruby_book/lib/yield.rb
+++ ruby_book/lib/yield.rb
@@ -275,3 +275,42 @@ question_proc = Proc.new { |text| "#{text}?" }
 
 # 3種類のPorcオブジェクトをgreetメソッドに渡す
 greet(shuffle_proc, repeat_proc, question_proc)
+
+# Proc.newまたはprocメソッドでProcオブジェクトを作成する
+Proc.new{ |a, b| a + b }
+proc { |a, b| a + b }
+
+# ->構文またはlambdaメソッドでProcオブジェクトを作成する
+->(a, b){ a + b }
+lambda{ |a, b| a + b }
+
+# Proc.newの作成と実行
+add_proc = Proc.new{ |a, b| a + b }
+add_proc.call(10, 20)
+
+# ラムダの作成と実行
+add_lambda = ->(a, b){ a + b }
+add_lambda.call(10, 20)
+
+# Proc.newの場合(引数がnilでもエラーがおきないようにto_iメソッドを使う)
+add_proc = Proc.new { |a, b| a.to_i + b.to_i }
+# Proc.newは引数が1つまたは3つでも呼び出しが可能
+add_proc.call(10)
+add_proc.call(10, 20, 100)
+
+# ラムダの場合
+add_lambda = ->(a,b){ a.to_i + b.to_i }
+# ラムダは個数について厳密なので、引数が2個ちょうどでなければエラーになる
+add_lambda.call(10)
+
+add_lambda.call(10, 20, 30)
+
+# Proc.newの場合
+add_proc = Proc.new { |a, b| a + b }
+add_proc.class
+add_proc.lambda?
+
+# ラムダの場合
+add_lambda = ->(a,b) { a + b }
+add_lambda.class
+add_lambda.lambda?
