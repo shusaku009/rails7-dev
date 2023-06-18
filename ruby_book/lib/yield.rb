27cd559b6c6f44e304493b5764308c5cd0816065
shusaku009 <shusaku009@gmail.com>
2023-06-18 16:03:34 +0900

    update yield

--- ruby_book/lib/yield.rb
+++ ruby_book/lib/yield.rb
@@ -249,3 +249,29 @@ greet(&repeat_proc)
 # &なしで呼び出すと普通の引数を1つ渡したことになる
 greet(repeat_proc)
 
+# ブロックではなく、1個のProcオブジェクトを引数として受け取る(&を付けない)
+def greet(arrange_proc)
+  puts 'おはよう'
+  text = arrange_proc.call('こんにちは')
+  puts text
+  puts 'こんばんは'
+end
+
+# Procオブジェクトを普通の引数としてgreetメソッドに渡す(&を付けない)
+repeat_proc = Proc.new{ |text| text * 2 }
+greet(repeat_proc)
+
+# 3種類のProcオブジェクトを受け取り、それぞれあいさつのじ列に適用するgreetメソッド
+def greet(proc_1, proc_2, proc_3)
+  puts proc_1.call('おはよう')
+  puts proc_2.call('こんにちは')
+  puts proc_3.call('こんばんは')
+end
+
+# greetメソッドに渡すProcオブジェクトを用意する
+shuffle_proc = Proc.new{ |text| text.chars.shuffle.join }
+repeat_proc = Proc.new{ |text| text * 2 }
+question_proc = Proc.new { |text| "#{text}?" }
+
+# 3種類のPorcオブジェクトをgreetメソッドに渡す
+greet(shuffle_proc, repeat_proc, question_proc)
