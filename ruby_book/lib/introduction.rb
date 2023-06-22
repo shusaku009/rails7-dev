7ee179ab7f502ab226e21dc2c0152d368be83668
shusaku009 <shusaku009@gmail.com>
2023-06-22 19:28:17 +0900

    update YAML

--- ruby_book/lib/introduction.rb
+++ ruby_book/lib/introduction.rb
@@ -83,3 +83,21 @@ JSON.parse(user_json)
 
 JSON.parse(user_json, symbolize_names: true)
 
+require 'yaml'
+
+# YAML形式のテキストデータを用意する
+yaml = <<TEXT
+alice:
+  name: 'Alice'
+  email: 'alice@example.com'
+  age: 20
+TEXT
+
+# YAMLテキストをパースしてハッシュに変換する
+users = YAML.load(yaml)
+
+# ハッシュに新しい要素を追加する
+users['alice']['gender'] = :female
+
+# ハッシュからYAMLテキストに変換する
+puts YAML.dump(users)
