8ca949ea6bec8e3fb5f57766a202be849b912a0b
shusaku009 <shusaku009@gmail.com>
2023-06-22 20:18:03 +0900

    update introduction

--- ruby_book/lib/introduction.rb
+++ ruby_book/lib/introduction.rb
@@ -115,3 +115,22 @@ age = ARGV[1]
 
 puts "name: #{name}, email: #{email}, age: #{age}"
 
+# 文字列としてRubyのコードを記述する
+code = '[1, 2, 3].map { |n| n * 10 }'
+
+# evalメソッドに渡すと、文字列がRubyのコードとして実行される
+eval(code)
+
+# OSのcatコマンドでテキストファイルの中身を表示する
+puts `cat lib/fizz_buzz.rb`
+
+puts %x{cat lib/fizz_buzz.rb}
+
+str = 'a, b, c'
+
+# str.upcaseを呼ぶのと同じ
+str.send(:upcase)
+
+# str.split(',')を呼ぶのと同じ
+str.send(:split, ',')
+
