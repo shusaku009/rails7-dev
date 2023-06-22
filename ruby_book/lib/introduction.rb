1d80812cfad703b7066dc84b5855f3feb9e8cd8a
shusaku009 <shusaku009@gmail.com>
2023-06-22 19:42:20 +0900

    update environmental_variables

--- ruby_book/lib/introduction.rb
+++ ruby_book/lib/introduction.rb
@@ -101,3 +101,17 @@ users['alice']['gender'] = :female
 
 # ハッシュからYAMLテキストに変換する
 puts YAML.dump(users)
+
+# 環境変数MY_NAMEの値を取得する
+name = ENV['MY_NAME']
+
+# 1番目と2番目の起動時引数を取得する
+email = ARGV[0]
+age = ARGV[1]
+
+name = ENV['MY_NAME']
+email = ARGV[0]
+age = ARGV[1]
+
+puts "name: #{name}, email: #{email}, age: #{age}"
+
